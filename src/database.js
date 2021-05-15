d13cce4b105ef4d1fd4ba723d22b1eff69226a0b
JOHN GONZALEZ <john_jairog@hotmail.com>
2021-05-15 15:55:32 -0600

    building commit #3

--- src/database.js
+++ src/database.js
@@ -2,7 +2,10 @@
 const mongoose = require('mongoose');
 
 const URI = 'mongodb://localhost/mern-task';
-mongoose.connect(URI)
+mongoose.connect(URI, 
+    {useNewUrlParser: true, 
+        useUnifiedTopology: true
+    })
 //hay que inicializar mongodb con 
 //mongod en el terminal.
     .then(db => console.log('Base de datos conectada'))
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -6,14 +6,49 @@ const router = express.Router();
 const Task_Model = require('../models/task');
 //Task_Model es el modelo de los datos en la DB
 
-Task_Model.find(function(err,tasks) {
-    console.log(tasks)
+//para consultar la DB:
+//Read (R)
+router.get('/', async(req,res)=>{
+    // res.send('Hola desde task.routes GET');
+    const tasks = await Task_Model.find();
+    // console.log(tasks);
+    // res.json('Respuestas desde router.get');
+    res.json(tasks);
 });
 
-router.get('/', (req,res)=>{
-    // res.send('Hola desde task.routes GET');
+//para insertar datos en la DB:
+//Create (C)
+router.post('/', async(req,res) => {
+    // console.log(req.body)
+    const { title, description} = req.body;
+    const tarea = new Task_Model ({title, description});
+    console.log(tarea);
+    await tarea.save();
     res.json({
-        status: 'API funcionando con JSON'
+        status: "Task saved"
+    })
+});
+
+//Para Editar datos en la DB:
+//Update (U)
+//Se necesita primero el id del item en la DB
+router.put('/:id', async(req,res) => {
+    const { title, description } = req.body;
+    const newTask = {title, description};
+    await Task_Model.findByIdAndUpdate(req.params.id, newTask)
+    res.json({
+        status: "Tarea actualizada."
+    });
+
+});
+
+//Para borrar datos de la BD:
+//Delete (D)
+//tambien se necesita el id
+router.delete('/:id', async(req,res) => {
+    await Task_Model.findByIdAndRemove(req.params.id);
+    res.json({
+        status: "Tarea Eliminada"
     });
 });
 
